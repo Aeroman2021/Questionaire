8bf7000f1300facb31ed25b0845e53e497303138
m_malakouti <malakouti.aero@gmail.com>
2022-04-17 15:20:01 +0430

    All crud methods except loadById added.

--- src/main/java/ir/malakouti/questionaire/service/QuestionService.java
+++ src/main/java/ir/malakouti/questionaire/service/QuestionService.java
@@ -1,4 +1,78 @@
 package ir.malakouti.questionaire.service;
 
-public class QuestionService {
+import ir.malakouti.questionaire.convertor.QuestionConvertor;
+import ir.malakouti.questionaire.exception.QuestionException;
+import ir.malakouti.questionaire.model.dto.QuestionDto;
+import ir.malakouti.questionaire.model.dto.QuestionRequestDto;
+import ir.malakouti.questionaire.model.entity.QuestionEntity;
+import ir.malakouti.questionaire.repo.QuestionRepository;
+import ir.malakouti.questionaire.service.core.AbstractCRUD;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService extends AbstractCRUD<QuestionEntity, Integer> {
+
+    private final QuestionRepository questionRepository;
+    private final QuestionConvertor questionConvertor;
+
+    @PostConstruct
+    public void init() {
+        setJpaRepository(questionRepository);
+    }
+
+    @Transactional
+    public QuestionDto saveQuestion(QuestionRequestDto questionRequestDto) {
+        try {
+            QuestionEntity question = QuestionEntity.builder()
+                    .title(questionRequestDto.getTitle())
+                    .description(questionRequestDto.getDescription())
+                    .build();
+            return questionConvertor.entityToDto(super.save(question));
+
+        } catch (QuestionException e) {
+            throw new QuestionException();
+        }
+    }
+
+    @Transactional
+    public QuestionDto updateQuestion(Integer questionId, QuestionDto questionDto) {
+
+        try{
+            QuestionEntity foundedQuestion = super.loadById(questionId);
+
+            if (questionDto.getDescription() != null) {
+                foundedQuestion.setDescription(questionDto.getDescription());
+            }
+
+            if (questionDto.getTitle() != null) {
+                foundedQuestion.setTitle(questionDto.getTitle());
+            }
+
+            return questionConvertor.entityToDto(super.update(foundedQuestion));
+
+        }catch (QuestionException e){
+            throw new QuestionException();
+        }
+    }
+
+
+    public List<QuestionDto> getAllQuestions(){
+        List<QuestionDto> questionDtos = new ArrayList<>();
+        for (QuestionEntity questionEntity : questionRepository.findAll()) {
+            QuestionDto questionDto = questionConvertor.entityToDto(questionEntity);
+            questionDtos.add(questionDto);
+        }
+        return questionDtos;
+    }
+
+
+
+
 }
