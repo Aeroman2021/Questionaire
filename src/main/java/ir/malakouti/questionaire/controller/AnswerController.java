69cfd6c373e712fbc6b250d9287039b1e8e63b66
m_malakouti <malakouti.aero@gmail.com>
2022-04-17 15:11:54 +0430

    update,loadall and loadbyId added.

--- src/main/java/ir/malakouti/questionaire/controller/AnswerController.java
+++ src/main/java/ir/malakouti/questionaire/controller/AnswerController.java
@@ -1,28 +1,28 @@
 package ir.malakouti.questionaire.controller;
 
 import ir.malakouti.questionaire.convertor.AnswerConvertor;
-import ir.malakouti.questionaire.model.dto.AnswerDto;
-import ir.malakouti.questionaire.model.dto.AnswerOutputDto;
-import ir.malakouti.questionaire.model.dto.AnswerRequestDto;
+import ir.malakouti.questionaire.model.dto.*;
+import ir.malakouti.questionaire.model.entity.AnswerEntity;
 import ir.malakouti.questionaire.service.AnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/answers")
+@RequestMapping("api/v1/answers")
 @RequiredArgsConstructor
 public class AnswerController {
 
     private final AnswerService answerService;
     private final AnswerConvertor answerConvertor;
 
-    @RequestMapping("/save")
+    @PostMapping("/save")
     public ResponseEntity<ResponseResult<AnswerOutputDto>> saveAnswer
-            (@RequestBody AnswerRequestDto answerRequestDto){
+            (@RequestBody AnswerRequestDto answerRequestDto) {
         AnswerDto result = answerService.saveAnswer(answerRequestDto);
         AnswerOutputDto answerOutputDto = answerConvertor.inputDtoToOutputDto(result);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -30,6 +30,52 @@ public class AnswerController {
                         .code(0)
                         .data(answerOutputDto)
                         .message("Answer added successfully...")
+                        .build());
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ResponseResult<AnswerOutputDto>> editAnswer
+            (@PathVariable("id") Integer id, @RequestBody AnswerDto answerDto) {
+        AnswerDto result = answerService.updateAnswer(id, answerDto);
+        AnswerOutputDto answerOutputDto = answerConvertor.inputDtoToOutputDto(result);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResponseResult.<AnswerOutputDto>builder()
+                        .code(0)
+                        .data(answerOutputDto)
+                        .message("Answer updated successfully...")
+                        .build());
+    }
+
+
+    @GetMapping
+    public ResponseEntity<ResponseResult<AnswerOutputDto>> getAnswerById(@PathVariable("id") Integer id) {
+        AnswerEntity result = answerService.loadById(id);
+        AnswerOutputDto answerOutputDto =
+                answerConvertor.inputDtoToOutputDto(answerConvertor.entityToDto(result));
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResponseResult.<AnswerOutputDto>builder()
+                        .code(0)
+                        .data(answerOutputDto)
+                        .message("Answer list loaded successfully...")
+                        .build());
+    }
+
+
+    @GetMapping
+    public ResponseEntity<ResponseResult<AnswerOutputDto>> getAllAnswers() {
+        List<AnswerOutputDto> answerOutputDtos = new ArrayList<>();
+        for (AnswerEntity answerEntity : answerService.loadAll()) {
+            AnswerOutputDto answerOutputDto =
+                    answerConvertor.inputDtoToOutputDto(answerConvertor.entityToDto(answerEntity));
+            answerOutputDtos.add(answerOutputDto);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResponseResult.<AnswerOutputDto>builder()
+                        .code(0)
+                        .dataList(answerOutputDtos)
+                        .message("Answer list loaded successfully...")
                         .build());
     }
 
