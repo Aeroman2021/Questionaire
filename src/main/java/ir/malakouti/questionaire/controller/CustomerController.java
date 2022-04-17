6e252bdfccee036c90e0892b93c230600d6bb5a4
m_malakouti <malakouti.aero@gmail.com>
2022-04-17 15:13:19 +0430

    creud methods added completely.

--- src/main/java/ir/malakouti/questionaire/controller/CustomerController.java
+++ src/main/java/ir/malakouti/questionaire/controller/CustomerController.java
@@ -2,25 +2,28 @@ package ir.malakouti.questionaire.controller;
 
 import ir.malakouti.questionaire.convertor.CustomerConvertor;
 import ir.malakouti.questionaire.model.dto.*;
+import ir.malakouti.questionaire.model.entity.CustomerEntity;
 import ir.malakouti.questionaire.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("api/v1/customers")
 @RequiredArgsConstructor
 public class CustomerController {
 
     private final CustomerService customerService;
     private final CustomerConvertor customerConvertor;
 
-    @RequestMapping("/save")
-    public ResponseEntity<ResponseResult<CustomerOutputDto>> saveAnswer
-            (@RequestBody CustomerRequestDto customerRequestDto){
+    @PostMapping("/save")
+    public ResponseEntity<ResponseResult<CustomerOutputDto>> saveCustomer
+            (@RequestBody CustomerRequestDto customerRequestDto) {
         CustomerDto result = customerService.registerCustomer(customerRequestDto);
         CustomerOutputDto customerOutputDto = customerConvertor.inputDtoToOutputDto(result);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -28,6 +31,50 @@ public class CustomerController {
                         .code(0)
                         .data(customerOutputDto)
                         .message("Customer registered successfully...")
+                        .build());
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ResponseResult<CustomerOutputDto>> editCustomer
+            (@PathVariable("id") Integer id, @RequestBody CustomerDto customerDto) {
+        CustomerDto result = customerService.editCustomer(id, customerDto);
+        CustomerOutputDto customerOutputDto = customerConvertor.inputDtoToOutputDto(result);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResponseResult.<CustomerOutputDto>builder()
+                        .code(0)
+                        .data(customerOutputDto)
+                        .message("Customer registered successfully...")
+                        .build());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseResult<CustomerOutputDto>> getCustomerById(@PathVariable("id") Integer id) {
+        CustomerEntity result = customerService.loadById(id);
+        CustomerOutputDto customerOutputDto =
+                customerConvertor.inputDtoToOutputDto(customerConvertor.entityToDto(result));
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResponseResult.<CustomerOutputDto>builder()
+                        .code(0)
+                        .data(customerOutputDto)
+                        .message("Customer list loaded successfully...")
+                        .build());
+    }
+
+
+    @GetMapping
+    public ResponseEntity<ResponseResult<CustomerOutputDto>> getAllCustomers() {
+        List<CustomerOutputDto> customerOutputDtos = new ArrayList<>();
+        List<CustomerDto> result = customerService.getAllCustomers();
+        for (CustomerDto customerDto : result) {
+            CustomerOutputDto customerOutputDto = customerConvertor.inputDtoToOutputDto(customerDto);
+            customerOutputDtos.add(customerOutputDto);
+        }
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResponseResult.<CustomerOutputDto>builder()
+                        .code(0)
+                        .dataList(customerOutputDtos)
+                        .message("Customer list loaded successfully...")
                         .build());
     }
 }
