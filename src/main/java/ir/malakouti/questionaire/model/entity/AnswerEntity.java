5c87b5290f5f849b85affea7c25b44327253b4ad
m_malakouti <malakouti.aero@gmail.com>
2022-04-20 11:12:54 +0430

    To fix infinte recursion, JasonIgnoreProperties add at the class level.

--- src/main/java/ir/malakouti/questionaire/model/entity/AnswerEntity.java
+++ src/main/java/ir/malakouti/questionaire/model/entity/AnswerEntity.java
@@ -1,9 +1,12 @@
 package ir.malakouti.questionaire.model.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Setter
@@ -12,6 +15,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @ToString
 @Builder
+@JsonIgnoreProperties(value = "customer")
 @Table(name = "answers")
 public class AnswerEntity {
 
@@ -22,11 +26,25 @@ public class AnswerEntity {
     @OneToOne
     private QuestionEntity question;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "customer_id")
     private CustomerEntity customer;
 
     @Column(nullable = false)
     private Integer rate;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AnswerEntity that = (AnswerEntity) o;
+        return question.equals(that.question) &&
+                customer.equals(that.customer) &&
+                rate.equals(that.rate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(question, customer, rate);
+    }
 }
