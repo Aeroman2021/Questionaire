ef68dd3a2c0ad63b3dd7143627c1cf1ec34f6ff3
m_malakouti <malakouti.aero@gmail.com>
2022-04-27 14:14:33 +0430

    FilterSetter added.

---
+++ src/main/java/ir/malakouti/questionaire/service/core/FilterSetter.java
@@ -0,0 +1,10 @@
+package ir.malakouti.questionaire.service.core;
+
+public class FilterSetter<T> {
+
+    private FilterData<T> filterData;
+
+    public FilterSetter(FilterData<T> filterData) {
+        this.filterData = filterData;
+    }
+}
